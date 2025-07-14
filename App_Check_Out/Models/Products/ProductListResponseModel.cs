581f7d6c56aa9f1b290ce40a9d0759b718db5463
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-14 14:38:36 +0700

    add

---
+++ App_Check_Out/Models/Products/ProductListResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using Entities.ViewModels.Products;
+
+namespace APP_CHECKOUT.Models.Models
+{
+    public class ProductListResponseModel
+    {
+        public List<ProductMongoDbModel> items { get; set; }
+        public long count { get; set; }
+    }
+    public class ProductListResponseFEModel
+    {
+        public List<ProductMongoDbModelFEResponse> items { get; set; }
+        public long count { get; set; }
+    }
+}
